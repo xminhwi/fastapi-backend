ed74d7513a71677d0ac1816d53ef4897685c02dd
xminhwi <abccccc55@yahoo.com.tw>
2024-11-14 16:14:59 +0800

    first commit

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name:str
+    price:float
+    description:str=None
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/item")
+def create_item(item:Item):
+    print(f"Received item:{item}")
+    return {"message":"Item received","item":item}
